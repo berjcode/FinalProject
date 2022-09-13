47b22f3e3fe422e250637d753b5c13a61ccb5342
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-09-14 01:39:04 +0300

    Update Core Layer

---
+++ Core/DataAcces/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAcces.EntityFramework
+{
+    public class EfEntityRepositoryBase
+    {
+    }
+}
