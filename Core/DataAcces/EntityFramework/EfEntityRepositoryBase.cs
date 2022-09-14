94273c551656467483ed3a283498001a08beed91
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-09-14 22:43:15 +0300

    Update

--- Core/DataAcces/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAcces/EntityFramework/EfEntityRepositoryBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.DataAcces.EntityFramework
 {
-    public class EfEntityRepositoryBase
+    public class EfEntityRepositoryBase<TEntity,TContext>
     {
     }
 }
